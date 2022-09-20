fd02d71298dcdc1a66fde8f378d14e27cca836c8
Ihor Matiushenko <igor.matiushenko@gmail.com>
2022-09-20 18:03:34 +0300

    [task-6] changed api paths after redeploy cloudfront on backend

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
   product:
-    'https://c78fxsssfa.execute-api.eu-west-1.amazonaws.com/dev/products',
+    'https://5aebpkytc1.execute-api.eu-west-1.amazonaws.com/dev/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://y4i9an8oj5.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://djzybsisld.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };

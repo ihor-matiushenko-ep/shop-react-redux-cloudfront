2ab0207290b71e60dafcc3d9729a4e81f8fcbb36
Ihor Matiushenko <igor.matiushenko@gmail.com>
2022-11-07 19:05:47 +0200

    [task-8] added cart url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://djzybsisld.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://ihor-matiushenko-ep-cart-api-development.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;

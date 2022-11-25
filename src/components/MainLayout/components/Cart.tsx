663dfc054f1c1ffb1af9fcc3c901bfb6ecb97d98
Ihor Matiushenko <ihormatiushenko@Ihors-MacBook-Pro.local>
2022-11-25 11:39:58 +0200

    [task-10] modify api path

--- src/components/MainLayout/components/Cart.tsx
+++ src/components/MainLayout/components/Cart.tsx
@@ -1,42 +1,45 @@
-import Badge from "@material-ui/core/Badge";
-import CartIcon from "@material-ui/icons/ShoppingCart";
-import IconButton from "@material-ui/core/IconButton";
-import React, { useEffect } from "react";
+import Badge from '@material-ui/core/Badge';
+import CartIcon from '@material-ui/icons/ShoppingCart';
+import IconButton from '@material-ui/core/IconButton';
+import React, { useEffect } from 'react';
 import axios from 'axios';
-import { useDispatch, useSelector } from "react-redux";
-import { selectCartItems, updateFromApi } from "store/cartSlice";
+import { useDispatch, useSelector } from 'react-redux';
+import { selectCartItems, updateFromApi } from 'store/cartSlice';
 import { Link } from 'react-router-dom';
-import API_PATHS from "../../../constants/apiPaths";
+import API_PATHS from '../../../constants/apiPaths';
 
 export default function Cart() {
   const dispatch = useDispatch();
   useEffect(() => {
-    axios.get(
-      `${API_PATHS.cart}/profile/cart`,
-      {
+    axios
+      .get(`${API_PATHS.cart}/cart/`, {
         headers: {
-          Authorization: `Basic ${localStorage.getItem('authorization_token')}`
+          Authorization: `Basic ${localStorage.getItem('authorization_token')}`,
+        },
+      })
+      .then(
+        ({
+          data: {
+            data: { cart },
+          },
+        }) => {
+          console.log('cart', cart);
+          dispatch(updateFromApi(cart));
         }
-      }
-    ).then(({ data: { data: { cart } } }) => {
-      console.log('cart',cart);
-      dispatch(updateFromApi(cart))
-    });
+      );
   }, [dispatch]);
   const cartItems = useSelector(selectCartItems);
   const badgeContent = cartItems.length || undefined;
 
-  
-
   return (
     <IconButton
-      aria-label="show 4 new mails"
-      color="inherit"
+      aria-label='show 4 new mails'
+      color='inherit'
       component={Link}
-      to="/cart"
+      to='/cart'
     >
-      <Badge badgeContent={badgeContent} color="secondary">
-        <CartIcon/>
+      <Badge badgeContent={badgeContent} color='secondary'>
+        <CartIcon />
       </Badge>
     </IconButton>
   );
--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,8 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://djzybsisld.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  // cart: 'http://ihor-matiushenko-ep-cart-api-development.eu-west-1.elasticbeanstalk.com/api',
-  cart: 'http://localhost:8000',
+  cart: 'http://ihor-matiushenko-ep-bff-api-develop.eu-west-1.elasticbeanstalk.com/',
 };
 
 export default API_PATHS;
